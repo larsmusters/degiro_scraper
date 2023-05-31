ad8505b17b26cbde9e39bf7eba6f518bb81b0e8f
larsmusters <lars.musters@hotmail.com>
2023-05-31 22:21:31 +0200

    working tutorial

---
+++ README.md
@@ -0,0 +1,13 @@
+Actions done in WSL:
+
+
+Tutorials: 
+
+General tutorial on selenium in python
+`https://www.scrapingbee.com/blog/selenium-python/`
+
+Installing chromedriver on WSL2 and code for proper direction in python
+`https://cloudbytes.dev/snippets/run-selenium-and-chrome-on-wsl2`
+
+Anti-scraping measures and how to avoid them
+`https://www.blog.datahut.co/post/web-scraping-how-to-bypass-anti-scraping-tools-on-websites`
---
+++ selenium_tut.py
@@ -0,0 +1,29 @@
+## Run selenium and chrome driver to scrape data from cloudbytes.dev
+import time
+import os.path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+## Setup chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless") # Ensure GUI is off
+chrome_options.add_argument("--no-sandbox")
+
+# Set path to chromedriver as per your configuration
+homedir = os.path.expanduser("~")
+webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
+
+# Choose Chrome Browser
+browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+# Get page
+browser.get("https://nintendo.com")
+
+text = browser.find_elements(By.XPATH, '//h2')
+print(text)
+
+#Wait for 10 seconds
+# time.sleep(10)
+browser.quit()
