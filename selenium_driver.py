c1afa8b29f9c78e9844a2bb0b10b2015a7cfab0e
larsmusters <lars.musters@hotmail.com>
2023-06-13 23:26:19 +0200

    make selenium class

---
+++ selenium_driver.py
@@ -0,0 +1,79 @@
+from selenium.webdriver.common.by import By
+from traceback import print_stack
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+from logger import get_logger
+
+
+class SeleniumDriver():
+    def __init__(self, driver):
+        self.driver = driver
+        self.logger = get_logger(__name__)
+
+    def getByType(self, locatorType):
+            locatorType = locatorType.lower()
+            if locatorType == "id":
+                return By.ID
+            elif locatorType == "name":
+                return By.NAME
+            elif locatorType == "xpath":
+                return By.XPATH
+            elif locatorType == "css":
+                return By.CSS_SELECTOR
+            elif locatorType == "class":
+                return By.CLASS_NAME
+            elif locatorType == "link":
+                return By.LINK_TEXT
+            else:
+                self.logger.critical("Locator type " + locatorType + " not correct/supported")
+            return False
+    
+    def getElement(self, locator, locatorType="id"):
+        element = None
+        try:
+            locatorType = locatorType.lower()
+            byType = self.getByType(locatorType)
+            element = self.driver.find_element(byType, locator)
+            if not element.is_displayed:
+                self.logger.critical("Element found is invisible, don't do anything with it: " + locator + " locatorType: " + locatorType)
+                raise NoSuchElementException
+            self.logger.info("Element Found with locator: " , locator , " and locatorType: " , locatorType)
+        except:
+            self.logger.critical("Element not found with locator: " , locator , " and  locatorType: " , locatorType)
+        return element
+    
+    def elementClick(self, locator, locatorType="id"):
+        try:
+            element = self.getElement(locator, locatorType)
+            element.click()
+            self.logger.info("Clicked on element with locator: " + locator + " locatorType: " + locatorType)
+        except:
+            self.logger.critical("Cannot click on the element with locator: " + locator + " locatorType: " + locatorType)
+            print_stack()
+
+    def sendKeys(self, data, locator, locatorType="id"):
+        try:
+            element = self.getElement(locator, locatorType)
+            element.send_keys(data)
+            self.logger.info("Sent data on element with locator: " + locator + " locatorType: " + locatorType)
+        except:
+            self.logger.info("Cannot send data on the element with locator: " + locator + " locatorType: " + locatorType)
+            print_stack()
+
+    def waitForElement(self, locator, locatorType="id",
+                               timeout=10, pollFrequency=0.5):
+        element = None
+        try:
+            byType = self.getByType(locatorType)
+            self.logger.info("Waiting for maximum :: " + str(timeout) + " :: seconds for element to be clickable")
+            wait = WebDriverWait(self.driver, timeout, poll_frequency=pollFrequency,
+                                 ignored_exceptions=[NoSuchElementException,
+                                                     ElementNotVisibleException,
+                                                     ElementNotSelectableException])
+            element = wait.until(EC.presence_of_element_located((byType, locator)))
+            self.logger.info("Element appeared on the web page")
+        except:
+            self.error.info("Element not appeared on the web page")
+            print_stack()
+        return element
--- selenium_tut.py
+++ selenium_tut.py
@@ -10,10 +10,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium_driver import SeleniumDriver
+from logger import get_logger
 
 load_dotenv()
 
-t = time.time()
 ## Setup chrome options
 chrome_options = Options()
 # chrome_options.add_argument("--headless") # Ensure GUI is off
@@ -27,84 +28,40 @@ chrome_options.add_experimental_option("prefs", {
 homedir = os.path.expanduser("~")
 webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
 
-# Choose Chrome Browser
+
+BASE_URL = "https://trader.degiro.nl/trader4/#/markets"
+
 browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+driver = SeleniumDriver(browser)
+driver.driver.get(BASE_URL)
 
-# Get page
-browser.get("https://trader.degiro.nl/trader4/#/markets")
+# # Wait for cookies and accept
+cookie_id = "CybotCookiebotDialogBodyButtonDecline"
+element = driver.waitForElement(cookie_id)
+driver.elementClick(cookie_id)
 
-# If there are cookies, accept them
-try: 
-    cookie_id = 'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll'
-    element = WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located((By.ID, cookie_id)))
-    accept_cookies = browser.find_element(By.ID, cookie_id).click()
-except NoSuchElementException:
-    print("couldn't find cookie element")
+# # Login sequence
+driver.sendKeys(os.getenv('DEGIRO_USERNAME'), '//*[@id="username"]', 'xpath')
+driver.sendKeys(os.getenv('DEGIRO_PASSWORD'), '//*[@id="password"]', 'xpath')
+driver.elementClick('//*[@id="loginForm"]/div[4]/button', 'xpath')
 
-# Login sequence
-try:
-    time.sleep(random.uniform(1,2))
-    login = browser.find_element(By.XPATH, '//*[@id="username"]')
-    if login.is_displayed():
-        login.send_keys(os.getenv('DEGIRO_USERNAME'))
-    else:
-        raise NoSuchElementException('Found an invisible username field?')
+# Check if logged in
+driver.waitForElement("//*[@id=\"appContainer\"]/div/aside/nav[2]/div/button", 'xpath')
 
-    time.sleep(random.uniform(1,2))
-    password = browser.find_element(By.XPATH, '//*[@id="password"]')
-    if password.is_displayed():
-        password.send_keys(os.getenv('DEGIRO_PASSWORD'))
-    else:
-        raise NoSuchElementException('Found an invisible password field?')
+# # Navigate to export page
+driver.elementClick('//*[@id=\"appContainer\"]/div/aside/nav[1]/a[3]', 'xpath')
 
-    time.sleep(random.uniform(1,2))
-    submit = browser.find_element(By.XPATH, '//*[@id="loginForm"]/div[4]/button')
-    if submit.is_displayed():
-        submit.click()
-    else:
-        raise NoSuchElementException('Found an invisible submit button?')
-except NoSuchElementException as e:
-    print("Some element was not found:", str(e))
+export_button_xpath = '//*[@id="mainContent"]/div[1]/section/div/section/div[1]/div/header/div/button'
+driver.waitForElement(export_button_xpath, 'xpath')
+driver.elementClick(export_button_xpath, 'xpath')
 
-# Test if logged in
-try:
-    time.sleep(2)
-    logout_button = browser.find_element(By.XPATH, "//*[@id=\"appContainer\"]/div/aside/nav[2]/div/button")
-    if not logout_button.is_displayed:
-        raise NoSuchElementException('Invisible Logout button')
-    print('Successfully logged in')
-except NoSuchElementException:
-    print('Incorrect login/password')
+csv_xpath = '/html/body/div[3]/div/div/div[2]/div/div[2]/a[3]'
+driver.waitForElement(csv_xpath, 'xpath')
+driver.elementClick(csv_xpath, 'xpath')
 
-# Navigate to export page
-try:
-    time.sleep(random.uniform(1,2))
-    portefeille_button_xpath = '//*[@id=\"appContainer\"]/div/aside/nav[1]/a[3]'
-    portefeuille_button = WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located((By.XPATH, portefeille_button_xpath)))
-    # portefeille_button = browser.find_element(By.XPATH, portefeille_button_xpath)
-    if not portefeuille_button.is_displayed:
-        raise NoSuchElementException('Invisible portefeuille button')
-    portefeuille_button.click()
-
-    time.sleep(random.uniform(1,2))
-    export_button_xpath = '//*[@id="mainContent"]/div[1]/section/div/section/div[1]/div/header/div/button'
-    export_button = WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located((By.XPATH, export_button_xpath)))
-    # export_button = browser.find_element(By.XPATH, export_button_xpath)
-    if not export_button.is_displayed:
-        raise NoSuchElementException('Invisible export button')
-    export_button.click()
-
-    time.sleep(random.uniform(1,2))
-    csv_xpath = '/html/body/div[3]/div/div/div[2]/div/div[2]/a[3]'
-    csv_button = WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located((By.XPATH, csv_xpath)))
-    # csv_button = browser.find_element(By.XPATH, csv_xpath)
-    if not csv_button.is_displayed:
-        raise NoSuchElementException('Invisible csv button')
-    csv_button.click()
-
-    WebDriverWait(browser, 10).until(lambda driver: ".csv" in browser.title)
-except NoSuchElementException:
-    print('Could not navigate to export page')
+#     WebDriverWait(browser, 10).until(lambda driver: ".csv" in browser.title)
+# except NoSuchElementException:
+#     print('Could not navigate to export page')
 
 time.sleep(10)
 browser.quit()
